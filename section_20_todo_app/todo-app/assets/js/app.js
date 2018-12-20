9aa062eef785bf2f9c9eb3b585abf5ea0caaacaf
Jason Bartley <jbartley11@gmail.com>
2018-12-19 21:33:04 -0600

    working through section 20 2

---
+++ section_20_todo_app/todo-app/assets/js/app.js
@@ -0,0 +1,30 @@
+// we want all current and future li's to have listener
+// but you can only use on on existing items
+
+// only listen to li's within ul
+$('ul').on("click", "li", function(){
+    $(this).toggleClass("completed")});
+
+// remove item
+$('ul').on("click", "span", function(){
+    // need to remove li
+    // since span is child of li we need to remove parent
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    // stop event from going up to parent elements
+    // since span is within li
+    event.stopPropagation();
+})
+
+// add new item to list
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        // get value
+        var task = $(this).val();
+        // make li
+        $('ul').append("<li><span>X</span> " + task + "</li>")
+        // clear input
+        $(this).val("");
+    }
+});
